9bd76b708ab1130bff33d8a6b0d02ae064498507
toa0904 <pazutomo0428@gmail.com>
2019-12-05 19:51:08 +0900

    定義の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @tweet = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    @tweet =current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end

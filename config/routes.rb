eb1e9a1acd8523b35d1902977cfd1a55b478873b
toa0904 <pazutomo0428@gmail.com>
2019-12-06 16:48:45 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
   resources :tweets do
   end
   resources :users, only: [:show]
-  post "likes/:food_id/create", to: "likes#create", constraints: {food_id: /\d+/}, as: :likes_create
-  post "likes/:food_id/delete", to: "likes#delete", constraints: {food_id: /\d+/}, as: :likes_delete
+  resources :likes, only: [:create, :destroy]
 end

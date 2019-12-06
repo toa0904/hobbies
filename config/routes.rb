eb375c40d13f67483c1b86a932956a75300ef87e
toa0904 <pazutomo0428@gmail.com>
2019-12-06 15:25:06 +0900

    rootingの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :tweets do
   end
   resources :users, only: [:show]
+  post "likes/:food_id/create", to: "likes#create", constraints: {food_id: /\d+/}, as: :likes_create
+  post "likes/:food_id/delete", to: "likes#delete", constraints: {food_id: /\d+/}, as: :likes_delete
 end

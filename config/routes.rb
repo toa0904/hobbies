dc9bc9dda1996dbe5388c00f85eace27acb67376
toa0904 <pazutomo0428@gmail.com>
2019-12-06 12:43:59 +0900

    resourcesの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
-  get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-  post  'tweets'      =>  'tweets#create' 
-  delete 'tweets/:id' =>   'tweets#destroy'
-  patch  'tweets/:id' =>   'tweets#update'
-  get   'tweets/:id/edit' => 'tweets#edit'
-  get   'tweets/:id'  =>   'tweets#show'
-  get   'users/:id'    =>  'users#show'       
-  # resources :tweets do
-  # end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html       
+  resources :tweets do
+  end
+  resources :users, only: [:show]
 end

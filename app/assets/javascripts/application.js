4252da1b70c6c5a8086010d93dfe3f902aef1c4d
toa0904 <pazutomo0428@gmail.com>
2019-12-08 16:18:55 +0900

    jsの追記

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
+//= require data-confirm-modal
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .

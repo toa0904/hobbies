58b901c310fa8fe4071a194eeb4de6be860e0d3e
toa0904 <pazutomo0428@gmail.com>
2019-12-08 17:23:03 +0900

    バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
-  has_many :likes
+  has_many :likes, dependent: :destroy
 end

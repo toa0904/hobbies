4aadb25b48d1af56bca539168d70c6f0da6a3ff7
toa0904 <pazutomo0428@gmail.com>
2019-12-08 17:24:27 +0900

    バリデーションの追記

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ApplicationRecord
   belongs_to :tweet
 
   validates :user_id, presencs: true
+  validates :tweet_id, presencs: true
+  validates_uniqueness_of :tweet_id, scope: :user_id
 end

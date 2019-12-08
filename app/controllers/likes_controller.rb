2c60fa2ed3aaa325a7500eb471e1cc09247ecc16
toa0904 <pazutomo0428@gmail.com>
2019-12-08 18:13:31 +0900

    修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,3 @@
 class LikesController < ApplicationController
 
-  def create
-    @like = Like.new(user_id: @current_user.id, tweet_id: params[:tweet_id])
-    @like.save
-    @tweet = Tweet.find_by(id: @like.tweet_id)
-    @like_count = Like.where(tweet_id: params[:tweet_id]).count
-  end
-
-  def delete
-    @like = Like.find_by(user_id: @current_user.id, tweet_id: params[:tweet_id])
-    @tweet = Tweet.find_by(id: @like.tweet_id)
-    @like.destroy
-    @like_count = Like.where(tweet_id: params[:tweet_id]).count
-  end
-
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,2 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :tweet, counter_cache: :likes_count
-
-  validates :user_id, presencs: true
-  validates :tweet_id, presencs: true
-  validates_uniqueness_of :tweet_id, scope: :user_id
 end
